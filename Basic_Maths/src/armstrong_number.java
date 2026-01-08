670a187a42df815df4417eee05d19558d1002690
JACKIE30 <your-email-linked-to-JACKIE30@github.com>
2026-01-09 00:44:54 +0530

    changes

---
+++ Basic_Maths/src/armstrong_number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class armstrong_number {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        int originalNumber=number;
+        int cubicNumber=0;
+        while(number!=0){
+            cubicNumber+=(int)Math.pow((number%10),3);
+            number/=10;
+        }
+        System.out.println(cubicNumber==originalNumber);
+    }
+}
