b28abea4847a1c92a5c8e9def623384b8853ce80
JACKIE30 <your-email-linked-to-JACKIE30@github.com>
2026-01-08 22:37:40 +0530

    messsage

--- Basic_Maths/src/count_digits.java
+++ Basic_Maths/src/count_digits.java
@@ -4,11 +4,7 @@ public class count_digits {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int number=sc.nextInt();
-        int count=0;
-        while(number!=0){
-            number/=10;
-            count++;
-        }
-        System.out.println(count);
+
+        System.out.println((int)Math.log10(number)+1);
     }
 }
